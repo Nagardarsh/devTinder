7860591f244efe248f0d28131ce0aaa45895eab6
Darshana-Nagar <darshananagar04@gmail.com>
2025-07-31 20:27:28 +0530

    reading param, query and dynamic routes

--- src/app.js
+++ src/app.js
@@ -1,35 +1,26 @@
-// console.log("Starting a new project");
-
 //importing express from node modules
 const express = require("express");
 
 //instance of an express js application
 const app = express();
 
-//request handler
-app.use("/hello", (req, res) => {
-    res.send("Hello hello hello");
-});
-//this will only handle GET call to /user
-app.get("/user", (req,res) => {
-    res.send({firstNmae: "Darshana", lastName: "nagar"});
-})
-
-app.post("/user", (req,res) => {
-    console.log("Save Data to the DB ");
-    res.send("data succesfully saved to the DB");
+// request handler
+app.get("/user", (req, res) => {
+    //this is how you read the query parameter
+    console.log(req.query);
+    res.send({firstName: "Darshana", lastName: "Nagar"});
 });
 
-app.delete("/user", (req,res) => {
-    res.send("Deleted data succesfully ");
-});
-//this will match all the HTTP method API calls to /test
-app.use("/test", (req, res) => {
-    res.send("Hello from the test!");
+app.get("/user/:userId", (req, res) => {
+    //this is how you read the param
+    console.log(req.params);
+    res.send({firstName: "Darshana", lastName: "Nagar"});
 });
 
-app.use("/", (req, res) => {
-    res.send("Hello Darshana!!!!");
+app.get("/user/:userId/:name/:password", (req, res) => {
+    //this is how you read the param
+    console.log(req.params);
+    res.send({firstName: "Darshana", lastName: "Nagar"});
 });
 
 app.listen(3000, () => {
